0f5d7c1d132848ab42639fbdc1a01ee55b6810f3
Jaime Valero <jaimevalero@Jaimes-MacBook-Pro.local>
2017-03-24 20:29:51 +0100

    First

---
+++ iris/iris-1.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+n_samples, n_features = iris.data.shape
+print(iris.keys())
+print((n_samples, n_features))
+print(iris.data.shape)
+print(iris.target.shape)
+print(iris.target_names)
+print(iris.feature_names)
---
+++ iris/iris-2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 'sepal width (cm)'
+x_index = 1
+# 'petal length (cm)'
+y_index = 2
+
+# this formatter will label the colorbar with the correct target names
+formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
+
+plt.scatter(iris.data[:, x_index], iris.data[:, y_index],
+            c=iris.target, cmap=plt.cm.get_cmap('RdYlBu', 3))
+plt.colorbar(ticks=[0, 1, 2], format=formatter)
+plt.clim(-0.5, 2.5)
+plt.xlabel(iris.feature_names[x_index])
+plt.ylabel(iris.feature_names[y_index]);
